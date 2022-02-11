e35fe149967834fdd45533b923e6d9096cef92e0
guangzan <guangzan1999@outlook.com>
2022-02-11 15:44:50 +0800

    auto deploy

--- src/build/leftSide/index.js
+++ src/build/leftSide/index.js
@@ -1,7 +1,7 @@
-import { useLinksOptions, useGithubOptions } from "@acnb/options";
-import { getBlogName } from "../../utils/cnblog";
-import { isOwner } from "../../utils/cnblog";
-import { avatar } from "../../constants/cnblog";
+import { useLinksOptions, useGithubOptions } from '@acnb/options'
+import { getBlogName } from '../../utils/cnblog'
+import { isOwner } from '../../utils/cnblog'
+import { avatar } from '../../constants/cnblog'
 import {
   cnblogHome,
   index,
@@ -9,114 +9,121 @@ import {
   send,
   rss,
   admin,
-} from "../../constants/links";
-import "./index.scss";
+  draftBox,
+} from '../../constants/links'
+import './index.scss'
 
 const buildLeftSidebar = () => {
-  const { enable } = useLinksOptions();
+  const { enable } = useLinksOptions()
   const el = $(`
     <div id='left-side'>
         <div class='logo'>
             <a href="https://www.cnblogs.com/">CNBLOG</a>
         </div>
-    </div>`);
+    </div>`)
   if (enable) {
     const links = $(`
         <div class="links side-wrapper">
             <h3>LINKS</h3>
             <ul></ul>
         </div>
-        `);
-    el.append(links);
+        `)
+    el.append(links)
   }
-  $("#home").append(el);
-};
+  $('#home').append(el)
+}
 
 const buildCustomLinks = () => {
-  const { enable, value } = useLinksOptions();
-  if (!enable) return;
+  const { enable, value } = useLinksOptions()
+  if (!enable) return
   for (const { name, link } of value) {
-    $("#left-side")
-      .find("ul")
-      .append(`<li><a href="${link}" target="_blank">${name}</a></li>`);
+    $('#left-side')
+      .find('ul')
+      .append(`<li><a href="${link}" target="_blank">${name}</a></li>`)
   }
-};
+}
 
 const removeHeaderToLeftSidebar = () => {
   const navList = [
     {
-      icon: "fa-blog",
-      title: "博客园",
+      icon: 'fa-blog',
+      title: '博客园',
       url: cnblogHome,
       allowVisit: true,
     },
     {
-      icon: "fa-home",
-      title: "首页",
-      url: index,
-      allowVisit: true,
-    },
-    {
-      icon: "fa-pen-square",
-      title: "新随笔",
+      icon: 'fa-pen-square',
+      title: '新随笔',
       url: newPost,
       allowVisit: true,
     },
     {
-      icon: "fa-envelope",
-      title: "联系",
+      icon: 'fa-paper-plane',
+      title: '草稿箱',
+      url: draftBox,
+      allowVisit: true,
+    },
+    {
+      icon: 'fa-home',
+      title: '首页',
+      url: index,
+      allowVisit: true,
+    },
+    {
+      icon: 'fa-envelope',
+      title: '联系',
       url: send,
       allowVisit: true,
     },
     {
-      icon: "fa-rss-square",
-      title: "订阅",
+      icon: 'fa-rss',
+      title: '订阅',
       url: rss,
       allowVisit: true,
     },
     {
-      icon: "fa-cog",
-      title: "管理",
+      icon: 'fa-cog',
+      title: '管理',
       url: admin,
       allowVisit: false,
     },
-  ];
+  ]
 
   const el = $(`
     <div id="cnblog-nav" class="side-wrapper">
         <h3>MENU</h3>
         <ul></ul>
     </div>
-    `);
+    `)
 
   for (const { icon, title, url, allowVisit } of navList) {
-    const target = title === "首页" ? "_self" : "_blank";
+    const target = title === '首页' ? '_self' : '_blank'
     const item = $(`<a href="${url}" target="${target}">
             <li>
-                <span class="fas ${icon}"></span>
+                <span class="fas fa-fw ${icon}"></span>
                 <span class="nav-item-text">${title}</span>
             </li>
-        </a>`);
+        </a>`)
 
-    if (!isOwner && !allowVisit) continue;
-    if (title === "订阅") {
-      item.removeAttr("target").attr({
-        "data-rss": url,
-        href: "javascript:void(0)",
+    if (!isOwner && !allowVisit) continue
+    if (title === '订阅') {
+      item.removeAttr('target').attr({
+        'data-rss': url,
+        href: 'javascript:void(0)',
         onclick: '$("#blog_nav_rss").trigger("click");',
-      });
+      })
     }
 
-    el.find("ul").append(item);
+    el.find('ul').append(item)
   }
 
-  $("#left-side .logo").after(el);
-};
+  $('#left-side .logo').after(el)
+}
 
 const buildBottomBtns = () => {
-  const { enable, url } = useGithubOptions();
-  if (!enable) return;
-  const userName = getBlogName();
+  const { enable, url } = useGithubOptions()
+  if (!enable) return
+  const userName = getBlogName()
   const el = `
     <div class="left-sidebar_bottom">
     <a href="${url}" class="follow-me" target="_blank">
@@ -126,13 +133,13 @@ const buildBottomBtns = () => {
             <span>${userName}</span>
         </span>
     </a>
-    </div>`;
-  $("#left-side").append(el);
-};
+    </div>`
+  $('#left-side').append(el)
+}
 
 export default () => {
-  buildLeftSidebar();
-  buildCustomLinks();
-  removeHeaderToLeftSidebar();
-  buildBottomBtns();
-};
+  buildLeftSidebar()
+  buildCustomLinks()
+  removeHeaderToLeftSidebar()
+  buildBottomBtns()
+}
--- src/constants/links.js
+++ src/constants/links.js
@@ -4,60 +4,47 @@
 import { getBlogName } from '../utils/cnblog'
 
 const { currentBlogApp } = window
-const blogname = getBlogName()
+const blogName = getBlogName()
 const home = 'https://home.cnblogs.com'
 const cnblog = 'https://www.cnblogs.com'
-
-/* 粉丝 */
-export const followersDetailsUrl = `${home}/u/${currentBlogApp}/followers/`
-
-/* 我的关注 */
-export const followingDetailsUrl = `${home}/u/${currentBlogApp}/followees/`
-
-/* 关注园友最新博文 */
-export const following = `${cnblog}/following`
-
-/* 我评 */
-export const commented = `${cnblog}/aggsite/mycommented`
-
-/* 我赞 */
-export const mydigged = `${cnblog}/aggsite/mydigged`
-
-/* 用户博客首页 */
-export const index = `${cnblog}/${currentBlogApp}`
-
-/* 博客园首页 */
-export const cnblogHome = 'https://www.cnblogs.com/'
-
-/* 新随笔 */
-export const newPost = 'https://i.cnblogs.com/posts/edit'
-
-/* 联系 */
-export const send = $('#blog_nav_contact').attr('href')
-
-/* 订阅 */
-export const rss = `https://www.cnblogs.com/${currentBlogApp}/rss/`
-
-/* 管理 */
-export const admin = 'https://i.cnblogs.com/'
+const iCnblog = 'https://i.cnblogs.com'
+const msg = 'https://msg.cnblogs.com'
 
 /* 用户详情页 */
-export const userDetails = `https://home.cnblogs.com/u/${currentBlogApp}/`
-
-/* 联系博主 */
-export const contact = `https://msg.cnblogs.com/send/${blogname}`
-
-/* 收藏 */
-export const appWz = 'https://wz.cnblogs.com/'
-
-/* 博问 */
-export const appQ = 'https://q.cnblogs.com/'
-
-/* 闪存 */
-export const appIng = 'https://ing.cnblogs.com/'
-
-/* 小组 */
-export const appGroup = 'https://group.cnblogs.com/'
-
+export const userDetails = `${home}/u/${currentBlogApp}`
+/* 粉丝 */
+export const followersDetailsUrl = `${home}/u/${currentBlogApp}/followers`
+/* 我的关注 */
+export const followingDetailsUrl = `${home}/u/${currentBlogApp}/followees`
+/* 博客园首页 */
+export const cnblogHome = cnblog
+/* 订阅 */
+export const rss = `${cnblog}/${currentBlogApp}/rss`
+/* 用户博客首页 */
+export const index = `${cnblog}/${currentBlogApp}`
+/* 我赞 */
+export const mydigged = `${cnblog}/aggsite/mydigged`
+/* 我评 */
+export const commented = `${cnblog}/aggsite/mycommented`
+/* 关注园友最新博文 */
+export const following = `${cnblog}/following`
+/* 新随笔 */
+export const newPost = `${iCnblog}/posts/edit`
+/* 联系 */
+export const send = $('#blog_nav_contact').attr('href')
+/* 管理 */
+export const admin = iCnblog
 /* 消息 */
-export const message = 'https://msg.cnblogs.com/'
+export const message = `${msg}`
+/* 联系博主 */
+export const contact = `${msg}/send/${blogName}`
+/* 收藏 */
+export const appWz = 'https://wz.cnblogs.com'
+/* 博问 */
+export const appQ = 'https://q.cnblogs.com'
+/* 闪存 */
+export const appIng = 'https://ing.cnblogs.com'
+/* 小组 */
+export const appGroup = 'https://group.cnblogs.com'
+/* 草稿箱 */
+export const draftBox = 'https://i.cnblogs.com/posts'
