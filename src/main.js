de837d4e6b7c3b26d6b08a99d5e21d11638f64df
guangzan <guangzan1999@outlook.com>
2022-07-14 00:01:32 +0800

    fix: tools mobile display

--- src/main.js
+++ src/main.js
@@ -80,27 +80,35 @@ createTheme()
     tools,
     { enable: true },
     {
-      menuIcon: '',
+      menuIconType: 'className',
+      menuIcon: "fa-angle-up",
+      menuActiveIcon: 'fa-angle-down',
       scrollContainer: '#mainContent',
       toolbarItems: [
         {
           icon: 'fa-rocket rocket-rotate',
+          iconType: 'className',
         },
         {
           enable: true,
           icon: 'fa-moon',
+          iconType: 'className',
         },
         {
           icon: 'fa-thumbs-up',
+          iconType: 'className',
         },
         {
           icon: 'fa-heart',
+          iconType: 'className',
         },
         {
           icon: 'fa-star',
+          iconType: 'className',
         },
         {
           icon: 'fa-comment-dots',
+          iconType: 'className',
         },
       ],
     }
