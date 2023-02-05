59f77e824142148ad2dc6c425a45fbb826101313
guangzan <guangzan1999@outlook.com>
2023-02-05 21:37:15 +0800

    pref(searchbar): opt style

--- src/modules/searchbar/index.js
+++ src/modules/searchbar/index.js
@@ -1,7 +1,7 @@
 import './index.scss'
 
 export function install() {
-  const el = '<div class="custom-searchbar"><input id="q" type="text" placeholder="Search..." onkeydown="return zzk_go_enter(event);"/></div>'
+  const el = '<div class="custom-searchbar"><input id="q" type="text" placeholder="search..." onkeydown="return zzk_go_enter(event);"/></div>'
   $('#main').prepend(el)
   $('#sidebar_search').remove()
 }
